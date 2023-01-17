04656144f987d5846793e6824fc176805f51a5c8
shurcaz <46389115+shurcaz@users.noreply.github.com>
2023-01-17 22:20:24 +0000

    Fixing file upload mess

--- Icarius/app/src/main/java/icarius/services/InvalidFileTypeException.java
+++
@@ -1,7 +0,0 @@
-package icarius.services;
-
-public class InvalidFileTypeException extends RuntimeException {
-    public InvalidFileTypeException (String message) {
-        super(message);
-    }
-}
