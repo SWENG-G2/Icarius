f76904dcda8a0ab809ddde533e28ea8c8bd3d189
gman98974 <goefronoscar@gmail.com>
2023-02-08 09:58:40 +0000

    Entities: Bird and Campus

--- app/src/main/java/icarius/entities/Campus.java
+++ app/src/main/java/icarius/entities/Campus.java
@@ -4,19 +4,31 @@ import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.dom4j.Node;
+
 import icarius.http.GetRequest;
+import icarius.http.DeleteRequest;
+import icarius.http.PatchRequest;
 import icarius.user.User;
+import java.util.HashMap;
+
 
 public class Campus extends ServerEntity {
 
     // Create campus object from existing database entry
-    public Campus(Long Id) {
+    public Campus(Long Id, String name) {
         super(Id);
     }
 
     // Create campus object and create new database entry
-    public Campus(String name, User user) {
+    public Campus(String name, icarius.user.User user) {
         super(name, user);
+    }
+
+    public Campus(Long id) {
+        super(id);
     }
 
     @Override
@@ -47,13 +59,44 @@ public class Campus extends ServerEntity {
 
     @Override
     protected void update(User user) {
-        // TODO Auto-generated method stub
-        
+        HashMap<String, String> params = new HashMap<>();
+        params.put("name", this.name);
+        PatchRequest request = new PatchRequest("/api/campus/update" , user);
+        request.addParameters(params);
+        request.send();
     }
 
     @Override
     protected Boolean delete(User user) {
         return delete("/api/campus/remove", user);
+    }
+
+    public class CampusList {
+        private List<Campus> campusList = new ArrayList<Campus>();
+
+        public CampusList() {
+            // send and store GET request response
+            GetRequest request = new GetRequest("/campus/list");
+            String response = request.send();
+
+            // Parse XML response into campus object list
+            try {
+                Document document = DocumentHelper.parseText(response);
+
+                List<Node> nodes = document.selectNodes("//*[name()='slide']");
+                for (Node node : nodes) {
+                    int id = Integer.parseInt(node.valueOf("@title"));
+                    String name = node.selectSingleNode("*[name()='text']").getText();
+                    campusList.add(new Campus(Id, name));
+                }
+            } catch (DocumentException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public List<Campus> getCampusList() {
+            return campusList;
+        }
     }
 
     @Override
